974ce608f5396a1d5d857714880330a2b6ed6a57
Matteo Visconti di Oleggio Castello <mvdoc@berkeley.edu>
2021-08-26 15:15:40 -0700

    ENH show example script to run permutation testing with clipped values

--- scripts/04stat/run_permtest_famdiff_bwsbj_bootstrap_singularity.sh
+++ scripts/04stat/run_permtest_famdiff_bwsbj_bootstrap_singularity.sh
@@ -6,26 +6,32 @@ BASEDIR=/backup/users/contematto/famface_angles
 IMG=$BASEDIR/singularity/$IMGNAME
 OUTDIR=$BASEDIR/derivatives103/slclfbwsbj-blockrun-deconvolve-hpalsid000005fsaverage6
 PYTHONWRAP="$BASEDIR"/scripts/python27
-NCORES=4
+NCORES=16
 
 HEMI=$1
 
-INPUT="$OUTDIR"/bwsbj_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-diff-identity_sl_000ip.niml.dset
-PERMUTED="$OUTDIR"/bwsbj_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-diff-identity_sl_*ip.niml.dset
-OUTPUT="$OUTDIR"/bwsbj_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-diff-identity_sl
+INPUT1="$OUTDIR"/bwsbj_task-fam1back_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-fam-identity_sl_000ip.niml.dset
+INPUT2="$OUTDIR"/bwsbj_task-str1back_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-str-identity_sl_000ip.niml.dset
+PERMUTED1="$OUTDIR"/bwsbj_task-fam1back_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-fam-identity_sl_*ip.niml.dset
+PERMUTED2="$OUTDIR"/bwsbj_task-str1back_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-str-identity_sl_*ip.niml.dset
+OUTPUT="$OUTDIR"/bwsbj_space-hpalsid000005fsaverage6_hemi-"$HEMI"_target-diff-identity-clipped_sl
 
 
 CMD="$BASEDIR/scripts/03stat/run_permtest_bootstrap.py \
-     -i $INPUT \
-     -p $PERMUTED \
+     --input $INPUT1 \
+     --input2 $INPUT2 \
+     --permuted $PERMUTED1 \
+     --permuted2 $PERMUTED2 \
      --prefix $OUTPUT \
      -n $NCORES \
-     --tail 1 \
+     --tail 0 \
      --nbootstraps 10000 \
+     --clip-min-value 0.25 \
      $CMD"
 
 
 singularity run  \
 -e \
+-B /backup \
 "$IMG" \
 "$PYTHONWRAP" "$CMD"
